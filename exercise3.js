d2d596e4fb61a572e96d86ea374f0e014f2cbae3
bibbi <elga.mancini@gmail.com>
2022-11-21 07:22:04 +0100

    modified with arrow function

--- exercise3.js
+++ exercise3.js
@@ -1,21 +1,16 @@
-const sum = function (a, b) {
-    return a + b;
-  }
+const sum = (a, b) => a + b;
   
-  const subtract = function (a, b) {
-    return a - b;
-  }
   
-  const multiply = function (a, b) {
-    return a * b;
-  }
+  const subtract = (a, b) => a - b;
   
-  const divide = function (a, b) {
-    return a / b;
-  }
   
-  const log = function (value) {
-    console.log(value);
-  }
+  const multiply = (a, b) => a * b;
+  
+  
+  const divide =  (a, b) => a / b;
+  
+  
+  const log = (value) => console.log(value);
+  
 
 log(divide(subtract(multiply(sum(2,4),sum(5,2)),2),5))
