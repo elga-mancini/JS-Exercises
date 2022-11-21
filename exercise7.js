a94d77633501656fc5b2487f8e1269614af43cae
bibbi <elga.mancini@gmail.com>
2022-11-21 11:42:43 +0100

    modified setTimeout

--- exercise7.js
+++ exercise7.js
@@ -1,13 +1,15 @@
 function printName() {
-    //la variabile deve essere dichiarata all'interno della funzione
-    const helloName = 'John';
-    //abbiamo accesso al global e printName scope
-    function inner() {
-        //abbiamo accesso al global, inner e printName scope
-      console.log(helloName);
-    }
-    //mandiamo a stampa la funzione inner
-    inner();
+  //la variabile deve essere dichiarata all'interno della funzione
+  const helloName = 'John';
+  //abbiamo accesso al global e printName scope
+  function inner() {
+    
+      //abbiamo accesso al global, inner e printName scope
+    return helloName;
   }
-//mandiamo a stampa la funzione
-setTimeout(printName, 1000);
+  //mandiamo a stampa la funzione inner
+  setTimeout(inner, 1000);
+  return inner();
+}
+//mandiamo a stampa la funzione principale
+console.log(printName())
