5b9e5142bca04a4f1e527642e47ea46c7c721eb7
bibbi <elga.mancini@gmail.com>
2022-12-01 16:26:19 +0100

    done

---
+++ cb.js
@@ -0,0 +1,8 @@
+let sayHello = () => console.log("Hello");
+
+function printAsyncName(callback, name) {
+  setTimeout(callback, 1000);
+  setTimeout(() => console.log(name), 2000);
+}
+
+printAsyncName(sayHello, "Alessandro");
